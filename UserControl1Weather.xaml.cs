9f7ca8fdcd38ad954f7dc30b9fb336c84fe9b295
Z1vran <156669756+fedotovaaaaa@users.noreply.github.com>
2024-09-23 14:29:19 +0300

    Initial commit

---
+++ UserControl1Weather.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1пр2.DataModel;
+namespace WpfApp1пр2
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1Weather.xaml
+    /// </summary>
+    public partial class UserControl1Weather : UserControl
+    {
+        public UserControl1Weather()
+        {
+            InitializeComponent();
+        }
+        public UserControl1Weather(Weather weather)
+        {
+            InitializeComponent();
+            System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["weatherViewSource2"];
+            myCollectionViewSource.Source = new List<Weather>() {weather};
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
